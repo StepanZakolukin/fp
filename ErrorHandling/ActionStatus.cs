aceaa76510bf4c723013eec587beb4cf214058ee
stepan <stepan.zakolyukin@yandex.ru>
2025-01-29 14:07:12 +0500

    Добавил класс-обертку над резудьтатом функции, невозвращающей значений

---
+++ ErrorHandling/ActionStatus.cs
@@ -0,0 +1,17 @@
+namespace ErrorHandling;
+
+public struct ActionStatus(string error)
+{
+    public string Error { get; } = error;
+    public bool IsSuccess => Error == null;
+    
+    public static ActionStatus Ok()
+    {
+        return new ActionStatus(null);
+    }
+
+    public static ActionStatus Fail(string e)
+    {
+        return new ActionStatus(e);
+    }
+}
